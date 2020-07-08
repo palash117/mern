4b8ba4df99df05165ec5e6e26c9e84dc196f3b80
palash117 <palash2392@gmail.com>
2020-07-08 10:21:12 +0530

    spacing

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-const config = require('config');
+const mongoose = require("mongoose");
+const config = require("config");
 
-const db = config.get('mongoURI');
+const db = config.get("mongoURI");
 // mongoose.connect(db);
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-    console.log('mongoDB connected ...');
-  } catch (err) {
-    console.log('error while connecting to db', err)
-    console.error(err.message);
-    process.exit(1);s
-  }
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log("mongoDB connected ...");
+	} catch (err) {
+		console.log("error while connecting to db", err);
+		console.error(err.message);
+		process.exit(1);
+		s;
+	}
 };
 
 module.exports = connectDB;
