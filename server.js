88a80ebad250ab98c36cf0214c713720df663ff5
palash117 <palash2392@gmail.com>
2020-07-13 15:55:43 +0530

    npm path

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 var app = express();
-
-
+cons path = require('path')
 
 connectDB();
 
+app.use(express.json({ extended: false }));
 
-app.use(express.json({extended: false}))
-app.get('/', (req, res) => {
-  console.log(req);
-  res.status(200);
-  res.send('api running');
-});
- 
 // DEFINE ROUTES
 // app.use('/api/users', (req, res , next)=>{ res.send('users'); next()})
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/posts', require('./routes/api/posts'))
-app.use('/api/auth', require('./routes/api/auth'))
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
+// Serve static assets in production
+if(process.env.NODE_ENV==='production'){
+  // Set static folder
+  app.use(express.static('client/build'))
+  app.get('*',(req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'bulid','index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
+	console.log(`server listening on port ${PORT}`);
 });
